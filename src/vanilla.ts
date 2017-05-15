1616f5e9892a6917564c9ae2424db530c468ee6e
Jeff Friesen <momoblackblack@gmail.com>
2017-05-15 15:53:55 -0600

    set up node webpack settings, no parallel-es yet

---
+++ src/vanilla.ts
@@ -0,0 +1,5 @@
+
+function add(a: number, b: number): number {
+  return a + 100
+}
+console.log('add: ', add(1, 2))
--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,43 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const DashboardPlugin = require('webpack-dashboard/plugin');
-const nodeEnv = process.env.NODE_ENV || 'development';
-const isProd = nodeEnv === 'production';
+const webpack = require('webpack')
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
 
-var config = {
-    devtool: isProd ? 'hidden-source-map' : 'cheap-eval-source-map',
-    context: path.resolve('./src'),
-    entry: {
-        app: './index.ts',
-        vendor: './vendor.ts'
-    },
-    output: {
-        path: path.resolve('./dist'),
-        filename: '[name].bundle.js',
-        sourceMapFilename: '[name].map',
-        devtoolModuleFilenameTemplate: function (info) {
-            return "file:///" + info.absoluteResourcePath;
-        }
-    },
-    module: {
-        rules: [
-            { enforce: 'pre', test: /\.ts$/, exclude: ["node_modules"], loader: 'ts-loader' },
-            { test: /\.html$/, loader: "html" },
-            { test: /\.css$/, loaders: ['style', 'css'] }
-        ]
-    },
-    resolve: {
-        extensions: [".ts", ".js"],
-        modules: [path.resolve('./src'), 'node_modules']
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': { // eslint-disable-line quote-props
-                NODE_ENV: JSON.stringify(nodeEnv)
-            }
-        }),
-        new HtmlWebpackPlugin({
-            title: 'Typescript Webpack Starter',
-            template: '!!ejs-loader!src/index.html'
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            minChunks: Infinity,
-            filename: 'vendor.bundle.js'
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false },
-            output: { comments: false },
-            sourceMap: false
-        }),
-        new DashboardPlugin(),
-        new webpack.LoaderOptionsPlugin({
-            options: {
-                tslint: {
-                    emitErrors: true,
-                    failOnHint: true
-                }
-            }
-        })
-    ]
-};
+const config = {
+  context: path.resolve('./src'),
+  entry: {
+    vanilla: './vanilla.ts',
+  },
+  output: {
+    path: path.resolve('./dist'),
+    filename: '[name].js',
+    libraryTarget: 'commonjs2',
+  },
+  devtool: 'source-map',
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: ['node_modules'],
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    modules: [path.resolve('./src'), 'node_modules'],
+  },
+  externals: [nodeExternals()],
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        tslint: {
+          emitErrors: true,
+          failOnHint: true,
+        },
+      },
+    }),
+  ],
+}
 
-module.exports = config;
+module.exports = config

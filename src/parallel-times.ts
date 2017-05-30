58b999d992c90ae6baef6469253a073054724c8f
Jeff Friesen <momoblackblack@gmail.com>
2017-05-30 17:11:52 -0600

    Documenting errors from `.times()` function

--- src/parallel-times.ts
+++ src/parallel-times.ts
@@ -1,22 +1,17 @@
 import parallel from 'parallel-es'
 
-const environment = { taskIndex: 2, valuesPerTask: 2 }
+/* Error
+* https://github.com/MichaReiser/parallel.es/blob/a00b50ae92cbfc1e4406c1cc72c113252d989afc/test/common/parallel/slave/times-iterator.specs.ts
+*
+* Error:
+* (node:32723) UnhandledPromiseRejectionWarning: Unhandled promise rejection (rejection id: 1): [object Object]
+* (node:32723) DeprecationWarning: Unhandled promise rejections are deprecated. In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code.
+*
+* The online solution to this problem is setting `"target": "es6"` but then I
+* can't use lamda functions in `.map` instead of explicit `function() {}`.
+*/
 
-// Can't get this working
-// https://github.com/MichaReiser/parallel.es/blob/a00b50ae92cbfc1e4406c1cc72c113252d989afc/test/common/parallel/slave/times-iterator.specs.ts
-
-
-// const generatorFunc = (n: number) => n
-// parallel
-//   .times(10, generatorFunc)
-//   .then(result => console.log(result))
-
-// parallel
-//   .times(5, 10, n => n * 2)
-//   .then(result => console.log(result))
-//
-
-// parallel.times(5, 2)
-// .map(value => value * value)
-// .subscribe((subresult, taskIndex) => console.log(`The result of the task ${taskIndex} is`, subresult))
-// .then(result => console.log(result))
+parallel.times(5, 2)
+  .map(value => value * value)
+  .subscribe((subresult, taskIndex) => console.log(`The result of the task ${taskIndex} is`, subresult))
+  .then(result => console.log(result))
